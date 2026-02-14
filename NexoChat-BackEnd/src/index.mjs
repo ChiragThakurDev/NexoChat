a6f2fed68cefead2ad6037949be88fc6566cbc84
Chirag <chirag212003@gmail.com>
2026-02-14 13:59:55 +0530

    Third-commit

--- NexoChat-BackEnd/src/index.mjs
+++ NexoChat-BackEnd/src/index.mjs
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["nexo-chat-lac.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
