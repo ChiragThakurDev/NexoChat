ca41649cd044a2f56eb175873b2b9f9836ed4292
Chirag Thakur <166230780+ChiragThakurDev@users.noreply.github.com>
2026-02-14 16:55:57 +0530

    Update index.mjs

--- NexoChat-BackEnd/src/index.mjs
+++ NexoChat-BackEnd/src/index.mjs
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["nexo-chat-lac.vercel.app", "http://localhost:5173"],
+    origin: ["https://nexo-chat-lac.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
