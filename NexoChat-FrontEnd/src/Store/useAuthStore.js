3767bce70d2ec58cafd8e05846a10b53214d77cf
Chirag <chirag212003@gmail.com>
2026-02-14 14:11:28 +0530

    Third-commit

--- NexoChat-FrontEnd/src/Store/useAuthStore.js
+++ NexoChat-FrontEnd/src/Store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "next-chat-self-three.vercel.app"
+    ? "https://next-chat-self-three.vercel.app"
     : "/";
 
 export const useAuthStore = create((set, get) => ({
--- NexoChat-FrontEnd/src/lib/axios.js
+++ NexoChat-FrontEnd/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "next-chat-self-three.vercel.app/api",
+  baseURL: "https://next-chat-self-three.vercel.app/api",
   withCredentials: true,
 });
