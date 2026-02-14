56a0aea6cb9dc0268fb6a508f40c33623c829bb5
Chirag Thakur <166230780+ChiragThakurDev@users.noreply.github.com>
2026-02-14 16:46:06 +0530

    Update socket.js

--- NexoChat-BackEnd/src/lib/socket.js
+++ NexoChat-BackEnd/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://nexo-chat-lac.vercel.app","http://localhost:5173", "http://localhost:5174"],
   },
 });
 export function getReceiverSocketId(userId) {
