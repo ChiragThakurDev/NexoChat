f5997db59e11e6892effeebbf28827c570ab6f2e
Chirag <chirag212003@gmail.com>
2026-02-14 13:12:12 +0530

    Third-commit

--- NexoChat-BackEnd/src/index.mjs
+++ NexoChat-BackEnd/src/index.mjs
@@ -15,6 +15,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
--- NexoChat-FrontEnd/src/lib/axios.js
+++ NexoChat-FrontEnd/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "next-chat-self-three.vercel.app/api",
   withCredentials: true,
 });
